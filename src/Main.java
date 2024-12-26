dca0f3eba3c95d6afefed692532f6571c7957071
mokou <mazename3@gmail.com>
2024-12-26 08:34:11 +0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+import java.util.*;
+import java.io.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        School school = new School();
+
+        // Process students
+        File studentsFile = new File("/home/mokou/IdeaProjects/students/src/students.txt");
+        Scanner studentScanner = new Scanner(studentsFile);
+        while (studentScanner.hasNextLine()) {
+            String[] studentData = studentScanner.nextLine().split(" ");
+            Student student = new Student(
+                    studentData[0], studentData[1], Integer.parseInt(studentData[2]),
+                    parseGender(studentData[3])
+            );
+            for (int i = 4; i < studentData.length; i++) {
+                student.addGrade(Integer.parseInt(studentData[i]));
+            }
+            school.addMember(student);
         }
+        studentScanner.close();
+
+        // Process teachers
+        File teachersFile = new File("/home/mokou/IdeaProjects/students/src/teachers.txt");
+        Scanner teacherScanner = new Scanner(teachersFile);
+        while (teacherScanner.hasNextLine()) {
+            String[] teacherData = teacherScanner.nextLine().split(" ");
+            Teacher teacher = new Teacher(
+                    teacherData[0], teacherData[1], Integer.parseInt(teacherData[2]),
+                    parseGender(teacherData[3]), teacherData[4],
+                    Integer.parseInt(teacherData[5]), Integer.parseInt(teacherData[6])
+            );
+            school.addMember(teacher);
+        }
+        teacherScanner.close();
+
+        // Output school members
+        System.out.println("School Members:");
+        System.out.println(school);
+    }
+
+    private static boolean parseGender(String gender) {
+        return gender.equalsIgnoreCase("Male");
     }
 }
